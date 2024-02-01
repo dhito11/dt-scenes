43b27a225b60be0d748adc9d4b7b37e32245095b
dt-dhito <153781142+dhito11@users.noreply.github.com>
2024-02-01 18:17:58 +0700

    Add files via upload

---
+++ dt-scenes/locales/en.lua
@@ -0,0 +1,7 @@
+local Translations = {
+    notify = {
+        ["laser_error"] = "Laser did not hit anything.",
+        ["scene_delete"] = "Scene deleted!",
+        ["scene_error"] = "No scene was close enough.",
+    }
+}
